799b8efd228d5866fa488bdb6cf8e0d3c6a9a4a1
OGWhiskers <bayandadlamini02@gmail.com>
2023-06-09 10:35:20 +0200

    Add basic functions
    Added basic functions for addition, multiplication, subtraction and
    division. For now result is console logged. Will return values in
    future.

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let sum = 0;
+let substracted = 0;
+let multiplyed = 1;
+let divided = 0;
+
+const add = (...a) => {
+  sum = 0;
+  for (let i = 0; i < a.length; i++) sum += a[i];
+
+  console.log(sum);
+};
+
+const multiply = (...mul) => {
+  multiplyed = 1;
+  for (let i = 0; i < mul.length; i++) multiplyed *= mul[i];
+
+  console.log(multiplyed);
+};
+
+const substract = (...sub) => {
+  console.log(sub);
+  substracted = 0;
+  for (let i = 0; i < sub.length - 1; i++) substracted += sub[i] - sub[i + 1];
+
+  console.log(substracted);
+};
+
+const divide = (...div) => {
+  console.log(div);
+  divided = 0;
+
+  for (let i = 0; i < div.length - 1; i++) divided += div[i] / div[i + 1];
+
+  console.log(divided);
+};
